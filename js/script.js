c22e0ba957d1d6cbe20f9deab00c4f5395180172
caio-clash <caio.9582567@aluno.mg.gov.br>
2026-03-24 18:21:23 +0000

    adicao do botao de deslike

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 let likeCount = 0;
+let desCount = 0;
+
+let curtido = false; // flag booleana
+let descurtido = false;
 
 function curtir() {
-  likeCount++;
-  document.getElementById("likeCount").innerText = likeCount;
+
+ if(curtido == false){
+    likeCount++;
+    curtido = true;
+    document.getElementById("likeCount").innerText = likeCount;
+ }else{
+    likeCount--;
+    curtido = false;
+    document.getElementById("likeCount").innerText = likeCount;
+ }
+}
+
+
+
+ function deslike () {
+if(descurtido == false){
+    desCount++;
+    descurtido = true;
+    document.getElementById("desCount").innerText = desCount;
+ }else{
+    desCount--;
+    descurtido = false;
+    document.getElementById("desCount").innerText = desCount;
+ }
+  
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("desBtn").addEventListener("click", deslike);
