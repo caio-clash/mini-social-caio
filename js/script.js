16074674d11f283f1a7f86fc9b49a3fbe1c6126f
caio-clash <caio.9582567@aluno.mg.gov.br>
2026-04-28 18:25:13 +0000

    alteracao nova

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
+// === ESTADO (dados da aplicação) === 
 let likeCount = 0;
 let dislikeCount = 0;
 let curtido = false;
 let descurtido = false;
 
-
-// === SERVICE(regras de negocio)
+// === SERVICE (regras de negócio)
 function curtir() {
   if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
+    
 
     if(descurtido == true){
       dislikeCount--;
       descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
     }
 
   }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
   }
 
 }
@@ -30,29 +27,43 @@ function descurtir() {
   if(descurtido == false){
     dislikeCount++;
     descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
+    
 
     if(curtido == true){
       likeCount--;
       curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
     }
 
   }
   else{
     dislikeCount--;
     descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
   }
 }
 
-//===CONTROLLER intermediacao evento/regra de negocio)
+//=== VIEWS (interface)===
 
-function clicarcurtir(){
-   curtir;
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
+}
+
+//=== CONTROLLER (intermediação evento/regras de negócio)
+
+function clicarCurtir(){
+  curtir();
+  atualizarTela();
+}
+function clicarDescurtir(){
+  descurtir();
+  atualizarTela();
 }
 
 
-//==  EVENTOS ===
-document.getElementById("likeBtn").addEventListener("click",clicarcurtir);
-document.getElementById("dislikeBtn").addEventListener("click", descurtir);
+// === EVENTOS ====
+document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
+document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO DE TELA ===
+
+atualizarTela();
